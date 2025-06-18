d5bdd4d2f9236599c79e3668434eb98d06749a61
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-06-18 22:13:07 +0900

    Initial commit

---
+++ Custom-SwiftUI-Craft/Custom_SwiftUI_CraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_SwiftUI_CraftApp.swift
+//  Custom-SwiftUI-Craft
+//
+//  Created by KimJunsoo on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Custom_SwiftUI_CraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
